ed1e8ac7e620611d65441a1754ca8e7060a79deb
Hana-kebede <hanakebedeemail@gmil.com>
2026-03-18 01:21:46 +0300

    Added Order model for order processing component

---
+++ backend/models/Order.js
@@ -0,0 +1,14 @@
+class Order {
+
+constructor(id, product, quantity, retailer){
+
+this.id = id
+this.product = product
+this.quantity = quantity
+this.retailer = retailer
+
+}
+
+}
+
+module.exports = Order
